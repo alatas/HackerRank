ec1995894d47e6237f952fb16b08e7391e6450dc
Sukru Alatas <alatas@gmail.com>
2016-07-10 21:10:48 +0300

    first commit - morgan and a string

---
+++ ConsoleTest/Bootstrapper.vb
@@ -0,0 +1,13 @@
+﻿Imports System.IO
+
+Module Bootstrapper
+    Sub Main()
+        Using sread As New StreamReader(AppContext.BaseDirectory & "..\..\..\HackerRank\In.txt", True),
+           swrite As New StreamWriter(AppContext.BaseDirectory & "\Out_code.txt", False)
+
+            Console.SetIn(sread)
+            Console.SetOut(swrite)
+            HackerRank.Main()
+        End Using
+    End Sub
+End Module
